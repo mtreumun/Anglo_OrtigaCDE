fa0dffc988f85844820124299f2bab0aba3d829a
mtreumun <48362157+mtreumun@users.noreply.github.com>
2023-11-10 02:43:54 -0300

    Third commit

--- Anglo_OrtigaCDE_MT_20231110_v1.R
+++ Anglo_OrtigaCDE_MT_20231110_v1.R
@@ -1,8 +1,16 @@
+#install.packages("sf")
+#install.packages("ggplot2")
+#install.packages("gridextra")
+#install.packages("classInt")
+#install.packages("gridExtra")
+
+
 # Cargar las librerías
 library(sf)
 library(ggplot2)
 library(gridExtra)
 library(classInt)
+library(gridExtra)
 
 # Leer el archivo shapefile
 shapefile_path <- "E:/ANGLO_Ortiga CDE/CAPAS/Unidades_vegetación_Ortiga_Ajuste_Dron_noviembre_2023_ME_MT_20231108_v1.shp"
@@ -56,28 +64,161 @@ labels_MEAN_per_area <- create_interval_labels(breaks_MEAN_per_area)
 labels_MEAN_normalized_STD <- create_interval_labels(breaks_MEAN_normalized_STD)
 
 # Crear mapas temáticos con etiquetas de intervalos
+#plot_shp_MEAN_cat <- ggplot(data = shp) +
+#geom_sf(aes(fill = cut(MEAN, breaks = breaks_MEAN, include.lowest = TRUE, labels = labels_MEAN)), color = NA) +
+#scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN)), labels = labels_MEAN) +
+#theme_minimal() +
+#labs(fill = "Rango", title = "Mapa de MEAN Categorizado") +
+#theme(legend.position = "bottom")
+
+#plot_shp_MEAN_per_area_cat <- ggplot(data = shp) +
+#geom_sf(aes(fill = cut(MEAN_per_area, breaks = breaks_MEAN_per_area, include.lowest = TRUE, labels = labels_MEAN_per_area)), color = NA) +
+#scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN_per_area)), labels = labels_MEAN_per_area) +
+#theme_minimal() +
+#labs(fill = "Rango", title = "Mapa de MEAN por Área Categorizado") +
+#theme(legend.position = "bottom")
+
+#plot_shp_MEAN_normalized_STD_cat <- ggplot(data = shp) +
+#geom_sf(aes(fill = cut(MEAN_normalized_STD, breaks = breaks_MEAN_normalized_STD, include.lowest = TRUE, labels = labels_MEAN_normalized_STD)), color = NA) +
+#scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN_normalized_STD)), labels = labels_MEAN_normalized_STD) +
+#theme_minimal() +
+#labs(fill = "Rango", title = "Mapa de MEAN Normalizado por STD Categorizado") +
+#theme(legend.position = "bottom")
+
+# Mostrar los mapas
+#print(plot_shp_MEAN_cat)
+#print(plot_shp_MEAN_per_area_cat)
+#print(plot_shp_MEAN_normalized_STD_cat)
+
+# Histograma para MEAN
+#hist_shp_MEAN <- ggplot(data = shp, aes(x = MEAN)) +
+  #geom_histogram(bins = 30, fill = "blue", color = "black") +
+  #theme_minimal() +
+  #ggtitle("Histograma NDVI")
+
+# Histograma para MEAN_per_area
+#hist_shp_MEAN_per_area <- ggplot(data = shp, aes(x = MEAN_per_area)) +
+  #geom_histogram(bins = 30, fill = "blue", color = "black") +
+  #theme_minimal() +
+  #ggtitle("Histograma de MEAN por Área")
+
+# Histograma para MEAN_normalized_STD
+#hist_shp_MEAN_normalized_STD <- ggplot(data = shp, aes(x = MEAN_normalized_STD)) +
+  #geom_histogram(bins = 30, fill = "blue", color = "black") +
+  #theme_minimal() +
+  #ggtitle("Histograma de MEAN Normalizado por STD")
+
+# Mostrar mapas y histogramas juntos
+#grid.arrange(plot_shp_MEAN_cat, hist_shp_MEAN, ncol = 2)
+#grid.arrange(plot_shp_MEAN_per_area_cat, hist_shp_MEAN_per_area, ncol = 2)
+#grid.arrange(plot_shp_MEAN_normalized_STD_cat, hist_shp_MEAN_normalized_STD, ncol = 2)
+
+# Crear mapas temáticos con etiquetas de intervalos
 plot_shp_MEAN_cat <- ggplot(data = shp) +
   geom_sf(aes(fill = cut(MEAN, breaks = breaks_MEAN, include.lowest = TRUE, labels = labels_MEAN)), color = NA) +
   scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN)), labels = labels_MEAN) +
   theme_minimal() +
-  labs(fill = "Rango", title = "Mapa de MEAN Categorizado") +
-  theme(legend.position = "bottom")
+  theme(
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 5, angle = 90), # Reducir tamaño y rotar las etiquetas de la leyenda
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    plot.margin = margin(5, 5, 5, 5),
+    axis.text.x = element_text(size = 6, angle = 90, hjust = 1, vjust = 0.5),
+    axis.text.y = element_text(size = 6, angle = 90, hjust = 1, vjust = 0.5),
+    axis.ticks = element_blank(),
+    legend.key.size = unit(1, 'lines'), # Ajustar tamaño de las cajas de leyenda para dar más espacio entre etiquetas
+    legend.spacing.x = unit(0.1, 'cm'), # Espaciado entre las leyendas
+    legend.margin = margin(t = 0, b = 0, unit = 'pt') # Reducir el margen alrededor de la leyenda
+  ) +
+  labs(fill = "", title = "Mapa NDVI")
 
+# Mejorar el histograma para MEAN
+hist_shp_MEAN <- ggplot(data = shp, aes(x = MEAN)) +
+  geom_histogram(bins = 30, fill = "blue", color = "black") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5),
+    axis.text.y = element_text(size = 6, angle = 90, vjust = 0.5),
+    #axis.text.y = element_blank(), # Ocultar las etiquetas del eje y para evitar repetición
+    axis.title.x = element_blank(), # Ocultar el título del eje x
+    axis.title.y = element_blank(), # Ocultar el título del eje y
+    plot.margin = margin(5, 5, 5, 5)
+  ) +
+  ggtitle("Histograma NDVI")
+
+# Combinar el mapa mejorado y el histograma en una sola visualización
+grid.arrange(plot_shp_MEAN_cat, hist_shp_MEAN, ncol = 2, widths = c(3, 2))
+
+# Mejorar el estilo del mapa para MEAN per Area
 plot_shp_MEAN_per_area_cat <- ggplot(data = shp) +
   geom_sf(aes(fill = cut(MEAN_per_area, breaks = breaks_MEAN_per_area, include.lowest = TRUE, labels = labels_MEAN_per_area)), color = NA) +
   scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN_per_area)), labels = labels_MEAN_per_area) +
   theme_minimal() +
-  labs(fill = "Rango", title = "Mapa de MEAN por Área Categorizado") +
-  theme(legend.position = "bottom")
+  theme(
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 5, angle = 90),
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    plot.margin = margin(5, 5, 5, 5),
+    axis.text.x = element_blank(), # Ocultar las etiquetas del eje x para evitar repetición
+    axis.text.y = element_blank(), # Ocultar las etiquetas del eje y para evitar repetición
+    axis.ticks = element_blank()
+  ) +
+  labs(fill = "", title = "Mapa NDVI Norm. Área")
 
+# Mejorar el histograma para MEAN per Area
+hist_shp_MEAN_per_area <- ggplot(data = shp, aes(x = MEAN_per_area)) +
+  geom_histogram(bins = 30, fill = "blue", color = "black") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5),
+    axis.text.y = element_text(size = 6, angle = 90, vjust = 0.5),
+    #axis.text.y = element_blank(), # Ocultar las etiquetas del eje y para evitar repetición
+    axis.title.x = element_blank(), # Ocultar el título del eje x
+    axis.title.y = element_blank(), # Ocultar el título del eje y
+  ) +
+  ggtitle("Histograma NDVI Norm. Área")
+
+# Combinar el mapa y el histograma para MEAN per Area
+grid.arrange(plot_shp_MEAN_per_area_cat, hist_shp_MEAN_per_area, ncol = 2, widths = c(3, 2))
+
+# Mejorar el estilo del mapa para MEAN Normalizado por STD
 plot_shp_MEAN_normalized_STD_cat <- ggplot(data = shp) +
   geom_sf(aes(fill = cut(MEAN_normalized_STD, breaks = breaks_MEAN_normalized_STD, include.lowest = TRUE, labels = labels_MEAN_normalized_STD)), color = NA) +
   scale_fill_manual(values = colorRampPalette(c("green", "red"))(length(labels_MEAN_normalized_STD)), labels = labels_MEAN_normalized_STD) +
   theme_minimal() +
-  labs(fill = "Rango", title = "Mapa de MEAN Normalizado por STD Categorizado") +
-  theme(legend.position = "bottom")
+  theme(
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.title = element_blank(),
+    legend.text = element_text(size = 5, angle = 90),
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    plot.margin = margin(5, 5, 5, 5),
+    axis.text.x = element_blank(), # Ocultar las etiquetas del eje x para evitar repetición
+    axis.text.y = element_blank(), # Ocultar las etiquetas del eje y para evitar repetición
+    axis.ticks = element_blank()
+  ) +
+  labs(fill = "", title = "Mapa NDVI Norm. por STD")
 
-# Mostrar los mapas
-print(plot_shp_MEAN_cat)
-print(plot_shp_MEAN_per_area_cat)
-print(plot_shp_MEAN_normalized_STD_cat)
+# Mejorar el histograma para MEAN Normalizado por STD
+hist_shp_MEAN_normalized_STD <- ggplot(data = shp, aes(x = MEAN_normalized_STD)) +
+  geom_histogram(bins = 30, fill = "blue", color = "black") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 9, hjust = 0.5, face = "bold"),
+    axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5),
+    axis.text.y = element_text(size = 6, angle = 90, vjust = 0.5),
+    #axis.text.y = element_blank(), # Ocultar las etiquetas del eje y para evitar repetición
+    axis.title.x = element_blank(), # Ocultar el título del eje x
+    axis.title.y = element_blank(), # Ocultar el título del eje y
+    plot.margin = margin(5, 5, 5, 5)
+  ) +
+  ggtitle("Histograma NDVI Norm. STD ")
+
+# Combinar el mapa y el histograma para MEAN Normalizado por STD
+grid.arrange(plot_shp_MEAN_normalized_STD_cat, hist_shp_MEAN_normalized_STD, ncol = 2, widths = c(3, 2))
